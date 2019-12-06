9df73d1e8feb996df6ccab70909d599d0186ff77
xaviranik <xaviranik.me@gmail.com>
2019-12-06 13:35:24 +0600

    Added: product collection for all products

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'basePrice' => $this->price,
+            'discountedPrice' => round($this->price - ($this->price * $this->discount / 100), 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'product' => route('products.show', $this->id)
+            ]
+        ];
     }
 }
